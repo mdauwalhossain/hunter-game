b04f9a36d5842424c1f33f5978eb92fa3a0bf38c
Gabriel Oczust <gabriel.oczust@gmail.com>
2021-02-28 19:23:37 -0300

    # Meta og:image

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ function MyApp({ Component, pageProps }) {
                 <meta property="og:site_name" content="Jackeline Oczust - Despachante de Armas" />
                 <meta property="og:type" content="website" />
                 <meta property="og:description" content="Quer ter sua arma de forma legalizada e não sabe como? Fale Conosco!" />
+                <meta property="og:image" content="https://instagram.fbfh15-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.97.780.780a/s640x640/152354238_113686284064662_3826999676540091630_n.jpg?tp=1&_nc_ht=instagram.fbfh15-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=vt2kQmHr2fQAX84x_GX&oh=42e0ffd12253f8d8de10c0075d65030a&oe=60653A84"></meta>
             </Head>
 
             <Header />
