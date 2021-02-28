10dc983fda3c86aaac901a9b08184ae702f0ff56
Gabriel Oczust <gabriel.oczust@gmail.com>
2021-02-28 19:23:12 -0300

    # Me - text correction

--- components/Me/index.jsx
+++ components/Me/index.jsx
@@ -9,7 +9,7 @@ export default function Me() {
                         Jackeline Oczust
                     </h1>
                     <p className={styles.text}>
-                        Formada em Direito, <b>instrutora de armamento</b>, tiro e tiro desportivo, ofereço o meu trabalho com excelência em todas as fases do processo para concessão do  seu <b>CR (Certificado de registro)</b> junto ao Exército, também prestando serviços na  renovação de CR ou 2° via, registro, apostilamento, Guia de trânsito, transferência SINARM - SIGMA.
+                        Formada em Direito, <b>Instrutora de armamento e tiro</b> e <b>Intrutora de tiro desportivo</b>, ofereço o meu trabalho com excelência em todas as fases do processo para concessão do  seu <b>CR (Certificado de registro)</b> junto ao Exército, também prestando serviços na  renovação de CR ou 2° via, registro, apostilamento, Guia de trânsito, transferência SINARM - SIGMA.
                         <br />
                         A experiência faz a diferença no processo, trabalhos com <b>excelente performance</b>, prazos e custos. Basicamente, o processo de concessão de CR funciona da seguinte forma: Cuidamos de toda parte burocrática, você só precisará nos fornecer as informações que iremos solicitando durante o processo, tudo muito bem explicado, com inicio, meio e fim, de forma <b>simplificada!</b>
                     </p>
