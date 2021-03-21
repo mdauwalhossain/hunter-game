d7f21c434fbc3e4b9152fe39ec72a975bb40e3f9
Gabriel Oczust <gabriel.oczust@gmail.com>
2021-03-21 15:18:40 -0300

    # Mobile - style improvements

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -1,16 +1,17 @@
-import styles from './Banner.module.css'
+import styles from "./Banner.module.css";
 
 export default function Banner() {
     return (
         <section className={styles.banner}>
             <div className="container">
                 <div className={styles.content}>
-
                     <div className={styles.title}>
                         <span>QUER TER</span>
                         <strong>SUA ARMA</strong>
                         <span>DE FORMA LEGALIZADA</span>
-                        <span>E <div>NÃO SABE COMO</div>?</span>
+                        <span>
+                            E <div>NÃO SABE COMO</div>?
+                        </span>
                     </div>
 
                     <a
@@ -19,7 +20,14 @@ export default function Banner() {
                         href="https://api.whatsapp.com/send?phone=5541988555153&text=Ol%C3%A1%2C%20entro%20em%20contato%20atrav%C3%A9s%20do%20seu%20site%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es"
                         target="_blank"
                     >
-                        Entre em Contato
+                        <span>Entre em Contato</span>
+                        
+                        <img
+                            src="/svgs/whatsapp.svg"
+                            alt="WhatsApp"
+                            width="18"
+                            height="18"
+                        />
                     </a>
 
                     <a
@@ -28,15 +36,21 @@ export default function Banner() {
                         href="https://wa.me/5541988555153?text=Ol%C3%A1%2C%20entro%20em%20contato%20atrav%C3%A9s%20do%20seu%20site%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es"
                         target="_blank"
                     >
-                        Entre em Contato
+                        <span>Entre em Contato</span>
+
+                        <img
+                            src="/svgs/whatsapp.svg"
+                            alt="WhatsApp"
+                            width="15"
+                            height="15"
+                        />
                     </a>
 
                     <span className={styles.description}>
                         Deixe a burocracia com a gente!
                     </span>
-
                 </div>
             </div>
         </section>
-    )
+    );
 }
